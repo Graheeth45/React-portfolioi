9eca86cf7de26332bb719fec43088b5cfe2f8d6a
ulaga7 <ulagadhoni.25@gmail.com>
2021-09-27 10:10:09 +0530

    Inital_commit

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Element} from 'react-scroll'
+
+const Contact = () => {
+    return (
+       <Element className="Contact" name="contact">
+           <h1>Hello</h1>
+       </Element>
+    )
+}
+
+export default Contact
