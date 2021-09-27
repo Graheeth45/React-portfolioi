5bef089ede78c1964ae758c9ab5dd8b0c10df4ac
ulaga7 <ulagadhoni.25@gmail.com>
2021-09-27 10:27:31 +0530

    third update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import About_me from './components/About_me/About_me';
+import Aboutme from './components/About_me/About_me';
 import Skills from './components/Skills/Skills';
 import Project from './components/Project/Project';
 import Hobbies from './components/Hobbies/Hobbies';
@@ -12,7 +12,7 @@ function App() {
   return (
     <div>
       <Header/>
-      <About_me/>
+      <Aboutme/>
       <Skills/>
       <Project/>
       <Hobbies/>
